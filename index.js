b625cf805326fa5a699c6b1631162aabc9dccfba
Chase Van Halen <cvanhalen@perscholas.org>
2022-10-25 12:42:02 -0500

    add getNumbers function

--- index.js
+++ index.js
@@ -37,6 +37,27 @@ getEven();
 // Fizz Buzz
 ////////////////////////////////
 
+const getMyNumbers = () => {
+    let num;
+    for(let i=1; i<101; i++){
+        num=i;
+    
+    if(num%3===0 && num%5===0){
+        console.log(`FizzBuzz`);
+    }
+    else if(num%3===0){
+        console.log(`Fizz`);
+    }
+    else if(num%5===0){
+        console.log(`Buzz`);
+    }
+    else {
+        console.log(num);
+    }
+}
+}
+
+getMyNumbers();
 
 ////////////////////////////////
 // Wild Wild Life
