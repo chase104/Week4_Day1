e329e8f66359208ab1d017981ae88680d1179de2
Chase Van Halen <cvanhalen@perscholas.org>
2022-10-25 12:50:00 -0500

    Wild Wild Life answered

--- index.js
+++ index.js
@@ -63,6 +63,25 @@ getMyNumbers();
 // Wild Wild Life
 ////////////////////////////////
 
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+
+// 1
+plantee[2] += 1;
+console.log(plantee)
+
+wolfy[3] = "Gotham City"
+
+dart[3] += " Hawkings"
+console.log(dart)
+
+wolfy.shift();
+wolfy.unshift('Gameboy');
+console.log(wolfy);
 
 ////////////////////////////////
 // Yell at the Ninja Turtles
